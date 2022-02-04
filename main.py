13a00d2e70d02b61d7f346f743c10677d52a06a0
Chandler Cain <chandler@clcain.com>
2022-02-04 11:03:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def root():
+    return 'Hello, world!', 200
+
 @app.route('/api/status', methods=['GET'])
 def api_status():
-    return 'HEALTHY', 200
+    return '', 200
 
 app.run(host='0.0.0.0', port=80)
